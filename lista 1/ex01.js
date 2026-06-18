39bf6b4b8fb255aecfc97d2bed17b4cf04ca92b9
victormartins012 <v.silvamartins2010@gmail.com>
2026-06-18 11:45:38 +0000

    lista 1

---
+++ lista 1/ex01.js
@@ -0,0 +1,2 @@
+/*Exercício 1 – Conversor de temperatura. Declare uma temperatura em Celsius numa variável. Converta
+para Fahrenheit usando a fórmula F = C * 1.8 + 32 e exiba o resultado com Template String.*/
