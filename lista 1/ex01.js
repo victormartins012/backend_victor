e9c32d93b6c56b2d50af08600e61821ef13fcb4b
victormartins012 <v.silvamartins2010@gmail.com>
2026-08-08 18:35:48 +0000

    oi

--- lista 1/ex01.js
+++ lista 1/ex01.js
@@ -4,4 +4,4 @@ let c = readline.questionFloat("Digite a temperatura a ser converida: ");
 
 let f = c * 1.8 + 32;
 
-console.log(`A temperaura em fahrenheit é ${f}`);
+console.log(`A temperaura em fahrenheit é ${f}`)
