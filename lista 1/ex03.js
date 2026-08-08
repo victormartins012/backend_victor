3f9158b63b977b4700c1c5cd850791dbc0298b70
victormartins012 <v.silvamartins2010@gmail.com>
2026-08-08 18:37:11 +0000

    oi

--- lista 1/ex03.js
+++ lista 1/ex03.js
@@ -4,4 +4,4 @@ const num = readline.questionInt('Digite um numero: ');
 
 let result = num % 2 === 0 ? "par" : "impar"; 
 
-console.log(`Seu numero é ${result}`)
+console.log(`Seu numero é ${result}`);
